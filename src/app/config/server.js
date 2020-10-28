aaf29177cba9b3de08e097f02d15d8d883c2002b
Carlos-SSH <carlos.anezeh@gmail.com>
2020-10-28 15:21:39 -0600

    Add ejs to this project.
    se anadio ejs a este proyecto.

--- src/app/config/server.js
+++ src/app/config/server.js
@@ -1,8 +1,14 @@
-const app = require('./config/server');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-require('./app/routes/users')(app);
+const app = express();
 
-//startingthe server
-app.listen(app.get('port'), () => {
-  console.log('Server on Port', app.get('port'));
-});
+// Ajustes
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../app/views'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,8 @@
-require('dotenv').config()
-const express = require("express"),
-    path = require("path"),
-    app = express(),
-    puerto = process.env.PORT || 3000;
+const app = require('./config/server');
 
-app.get('/', (peticion, respuesta) => {
-    let agenteDeUsuario = peticion.header("user-agent");
-    respuesta.send("La ruta / solicitada con: " + agenteDeUsuario);
-});
-app.get('/pagina', (peticion, respuesta) => {
-    let rutaDeArchivo = path.join(__dirname, "plantilla.html");
-    respuesta.sendFile(rutaDeArchivo);
-});
+require('./app/routes/users')(app);
 
-app.get('/hola', (peticion, respuesta) => {
-    let mascota = {
-        nombre: "Maggie",
-        edad: 2,
-    };
-    respuesta.json(mascota);
-});
-
-// Una vez definidas nuestras rutas podemos iniciar el servidor
-app.listen(puerto, err => {
-    if (err) {
-        // Aquí manejar el error
-        console.error("Error escuchando: ", err);
-        return;
-    }
-    // Si no se detuvo arriba con el return, entonces todo va bien ;)
-    console.log(`Escuchando en el puerto :${puerto}`);
+//startingthe server
+app.listen(app.get('port'), () => {
+  console.log('Server on Port', app.get('port'));
 });
